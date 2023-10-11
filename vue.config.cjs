28da54355c74839b099d448027e8b7ad32372db2
Azriel Attaufiqi <azrielrisywan@gmail.com>
2023-10-11 17:04:11 +0700

    update config.cjs

--- vue.config.cjs
+++ vue.config.cjs
@@ -1,7 +1,4 @@
 const { fileURLToPath, URL } = require('url');
-const { createRequire } = require('module');
-
-const require = createRequire(import.meta.url);
 
 module.exports = {
   transpileDependencies: true,
