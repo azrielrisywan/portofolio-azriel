9b2f8feb1bd8814c2ff77cd892216ab87dc94b68
Azriel Attaufiqi <azrielrisywan@gmail.com>
2023-10-11 17:00:24 +0700

    update config.cjs

--- vue.config.cjs
+++ vue.config.cjs
@@ -1,7 +1,4 @@
 const { fileURLToPath, URL } = require('url');
-const { createRequire } = require('module');
-
-const require = createRequire(import.meta.url);
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
